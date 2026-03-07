8939c75f0647194d9dede8ea112bd30e9d46171d
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-07 15:02:24 +0530

    commited

--- peering.tf
+++ peering.tf
@@ -35,13 +35,13 @@ resource "aws_route" "public_peering" {
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id = aws_route_table.private.id
-  destination_cidr_block = data.aws_vpc.default.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0  
   route_table_id = aws_route_table.database.id
-  destination_cidr_block = data.aws_vpc.default.id
+  destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
