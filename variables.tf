e4d1c1719d53798a1e35f75ef4579d7bd35f4b06
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-08 12:08:43 +0530

    commited

--- variables.tf
+++ variables.tf
@@ -25,25 +25,25 @@ variable "public_subnet_cidr" {
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+variable "private_subnet_cidr" {
+  type = list 
+  default = ["10.0.11.0/24" , "10.0.12.0/24"]
+}
+
+variable "database_subnet_cidr" {
+  type = list 
+  default = ["10.0.21.0/24" , "10.0.22.0/24"]
+}
+
 
 variable "public_subnet_tags" {
   type = map 
   default = {}
 }
 
-variable "private_subnet_cidr" {
-  type = list 
-  default = ["10.0.11.0/24" , "10.0.12.0/24"]
-}
-
 variable "private_subnet_tags" {
   type = map 
   default = {}
-}
-
-variable "database_subnet_cidr" {
-  type = list 
-  default = ["10.0.21.0/24" , "10.0.22.0/24"]
 }
 
 variable "database_subnet_tags" {
